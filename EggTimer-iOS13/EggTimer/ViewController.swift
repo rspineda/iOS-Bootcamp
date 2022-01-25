d6329154179bfae6f8100d166926dfac634bed1f
Ronald Pineda <ronystivens@gmail.com>
2022-01-25 12:26:04 +0100

    added timers and labels

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    @IBOutlet weak var titleLable: UILabel!
+    
+    var defaultValue = "ninguno"
+    var secondsRemaining = 60
+    var timer = Timer()
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLable.text = "DONE!"
+        }
+    }
+    
 
 }
