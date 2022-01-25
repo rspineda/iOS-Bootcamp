6e06ae676b5e8623af8b93c1592ab7f858bdea2b
Ronald Pineda <ronystivens@gmail.com>
2022-01-25 13:48:16 +0100

    added progress bar and attached to labels

--- EggTimer-iOS13/EggTimer/ViewController.swift
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -11,28 +11,39 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLable: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     var defaultValue = "ninguno"
-    var secondsRemaining = 60
+    var totalTime = 0
+    var secondsPassed = 0
     var timer = Timer()
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        progressBar.progress = 0
         timer.invalidate()
         let hardness = sender.currentTitle!
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLable.text = hardness
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            let percentageProgress = Float(secondsPassed) / Float(totalTime)
+            progressBar.progress = percentageProgress
         } else {
             timer.invalidate()
             titleLable.text = "DONE!"
         }
+    }
+    
+    func progressTimeCalculator() {
+        
     }
     
 
