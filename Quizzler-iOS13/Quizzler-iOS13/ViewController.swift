6c3e2af0d1d1d0d47a8f5bf14dcc943b01d85efa
Ronald Pineda <ronystivens@gmail.com>
2022-01-28 23:55:21 +0100

    added quiz validation

--- Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        ["4 + 2 is equal to 6", "True"],
+        ["5 - 3 is greater than 1", "True"],
+        ["3 + 8 is less than 10", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Right")
+        } else {
+            print("Wrong")
+        }
+        
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
+    
 
 }
 
