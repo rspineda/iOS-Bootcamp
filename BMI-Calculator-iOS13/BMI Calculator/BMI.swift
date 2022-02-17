ebc91408e20ce31e342dae7b5f5f50bd6d6b922d
Ronald Pineda <ronystivens@gmail.com>
2022-02-17 13:17:16 +0100

    added BMI messages logic based on bmi calculation

---
+++ BMI-Calculator-iOS13/BMI Calculator/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Ronald Pineda on 17/2/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
--- BMI-Calculator-iOS13/BMI Calculator/CalculatorBrain.swift
+++ BMI-Calculator-iOS13/BMI Calculator/CalculatorBrain.swift
@@ -6,18 +6,26 @@
 //  Copyright © 2022 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi: BMI?
     
     mutating func calculateBMI(height: Float, weight: Float) {
-        bmi = weight / pow(height, 2)
+        let bmiValue = weight / pow(height, 2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "Take some tapas man!", color: UIColor.blue)
+        } else if bmiValue < 24.9 {
+            bmi = BMI(value: bmiValue, advice: "Nice job, you are on shape!", color: UIColor.green)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Too mnay tapas man!, do some sports!", color: UIColor.red)
+        }
     }
     
     func getBMIValue() -> String {
-        let bmiTo1DecimalPlace = String(format: "%.1f", bmi)
+        let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
         return bmiTo1DecimalPlace
     }
 }
